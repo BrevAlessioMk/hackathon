88b06411c2fbdec75248a1b5152264da052aef61
Alessio Breviglieri <alessio.breviglieri@motork.io>
2023-07-05 09:21:06 +0200

    fix endpoint

--- src/services/TemperatureService.js
+++ src/services/TemperatureService.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost',
+  baseURL: 'http://localhost:9999',
 });
 
 export class TemperatureService {
   static async getTemperature(vehicleData) {
     try {
-      const temperature = await httpClient.get('/temperature', {
-        params: { ...vehicleData },
-      });
-      return temperature;
+      const temperature = await httpClient.post('/routes/orchestrator/endpoints/hackathon/predict', vehicleData);
+      return temperature.data.prediction;
     } catch {
       return null;
     }
